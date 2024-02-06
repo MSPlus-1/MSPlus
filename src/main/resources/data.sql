6f03cee211fe21dc83da3fdc684fe4e7a8dcf1f0
이윤재 <yunjea031296@gmail.com>
2024-02-06 19:39:09 +0900

    feat(#1): 테이블 스키마 및 더미 데이터 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO `member` (`id`, `member_id`, `name`, `email`, `created_at`) VALUES (1, 'hama', '하마', 'mailtest@msplus.com', '2024-02-06 00:00:00');
+INSERT INTO `member` (`id`, `member_id`, `password`, `name`, `email`, `nickname`, `hp`, `created_at`, `updated_at`, `deleted_at`) VALUES (1, 'hama', 'abc123', '하마', 'mailtest@msplus.com', '하마', '010-1111-2222', '2024-02-06 00:00:00', '2024-02-06 00:00:00', NULL);
 
 INSERT INTO `category` (`id`, `name`, `created_at`, `updated_at`, `deleted_at`) VALUES (1, 'category1', '2024-02-06 00:00:00', '2024-02-06 00:00:00', NULL);
