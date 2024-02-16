8ab6801960fff4fc8686381af1ec26a6fc7ec8d5
이윤재 <yunjea031296@gmail.com>
2024-02-16 17:52:19 +0900

    feat: 게시판 생성 테스트 코드 추가

--- src/test/java/com/part3/msplus/board/command/application/BoardCreateServiceTest.java
+++ src/test/java/com/part3/msplus/board/command/application/BoardCreateServiceTest.java
@@ -1,7 +1,43 @@
 package com.part3.msplus.board.command.application;
 
+import com.part3.msplus.board.command.domain.repository.BoardRepository;
+import com.part3.msplus.board.controller.dto.request.BoardCreateRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class BoardCreateServiceTest {
 
+    @Autowired
+    BoardCreateService boardCreateService;
+
+    @Autowired
+    BoardRepository boardRepository;
+
+    @Test
+    @DisplayName("게시글 생성")
+    void createBoard() {
+        // given
+        BoardCreateRequest boardCreateRequest = makeRequest();
+
+        // when
+        boardCreateService.createBoard(boardCreateRequest);
+
+        // then
+        assertThat(boardRepository.findAll()).hasSize(1);
+    }
+
+    private BoardCreateRequest makeRequest() {
+        return BoardCreateRequest.builder()
+                .memberId(1L)
+                .categoryId(1L)
+                .boardName("게시글 제목")
+                .build();
+    }
 }
